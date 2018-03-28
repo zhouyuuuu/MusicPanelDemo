3020b5980e70575b267e5a2e6b69437d56abcc5e
周宇 <zy22@email.com>
2018-03-28 20:21:07 +0800

    [Feature]布局适配了普通720p,5英寸1080p,6英寸1080p

--- app/src/main/java/com/example/administrator/musiceditingpanelproject/activity/LaunchActivity.java
+++ app/src/main/java/com/example/administrator/musiceditingpanelproject/activity/LaunchActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.administrator.musiceditingpanelproject.R;
+import com.example.administrator.musiceditingpanelproject.application.MusicEditingPanelApplication;
 
 /**
  * Edited by Administrator on 2018/3/28.
@@ -21,6 +23,12 @@ public class LaunchActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_launch);
         TextView mTvLaunch = findViewById(R.id.tv_launch);
         mTvLaunch.setOnClickListener(this);
+        mTvLaunch.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("!!!!!", "run: " + MusicEditingPanelApplication.getApplication().getResources().getDisplayMetrics().density);
+            }
+        },300);
     }
 
     @Override
